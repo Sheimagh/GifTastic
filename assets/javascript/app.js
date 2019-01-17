0196185416954da46cd6bb21045005f454f02074
sheimagh <info@sheima.com>
2019-01-16 21:41:58 -0600

    fixed the issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,86 @@
+//Creates an array called tvShows
 var tvShows = ["Desperate Housewives", "Daria", "South Park", "Friends", "Buffy the Vampire Slayer", "Saved by the Bell", "Perfect Strangers", "Full House", "The Ren & Stimpy Show", "The Powerpuff Girls", "Doctor Who"];
 
-function renderButtons() {
-	$("#buttonsArea").empty(); // empties the buttonsArea div so we don't make duplicates
+// function for displaying movie data, then deleting the movies-(6.2-WorkingMovieApp)
+function buttons() {
+	$("#buttonsArea").empty();
 
-	// creates a button with attributes for every item in the tvShows array
-	for (var i = 0; i < tvShows.length; i++) {
-		var button = $("<button>");
-		button.html(tvShows[i]);
-		button.addClass("btn btn-outline-secondary");
-		button.attr("id", "tv-btn");
-		button.attr("tv-title", tvShows[i]);
-		$("#buttonsArea").append(button);
+// Looping through the array of tvShows-(6.2-WorkingMovieApp)
+for (var i = 0; i < tvShows.length; i++) {
+	var button = $("<button>");
+	button.html(tvShows[i]);
+	button.addClass("btn btn-outline-secondary");
+	button.attr("id", "tv-btn");
+	button.attr("tv-title", tvShows[i]);
+	$("#buttonsArea").append(button);
 	}
 }
 
+// displayGifs function re-renders the HTML to display the appropriate content-(6.2-WorkingMovieApp)
 function displayGifs() {
 	var thisShow = $(this).attr("tv-title");
 	console.log(thisShow);
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thisShow + "&api_key=O3EUoUzS6MC98XOKeTv3sJgz3m93BJFE&limit=10";
 
-	// ajax call that gets and returns the response object from the query url
+	// Performing an AJAX with the queryURL-the Get Method(6.3-dynamic-elements or 6.1-giphy-api)
 	$.ajax({
 		url: queryURL,
 		method: "GET"
-	}).done(function(response) {
+
+	// After data comes back from the request
+	}).then(function (response) {
 		console.log(response);
-		var response = response.data;
+		var results = response.data;
 
-		// creates a div that contains a still image gif and rating info for each response item
-		for (var i = 0; i < response.length; i++) {
-			var gifDiv = $("<div>");
-			gifDiv.addClass("gifDiv");
+	// looping through the movies
+	for (var i = 0; i < results.length; i++) {
+	var gifDiv = $("<div>");
+	gifDiv.addClass("gifDiv");
 
-			var rating = response[i].rating;
-			var p = $("<p>").html("Rating: " + rating);
-			p.addClass("text-center");
+	//Creating a line with the animation's rating-(6.3-DynamicElements)
+	var p = $("<p>").text("Rating: " + results[i].rating);
 
-			var gifImage = $("<img>");
-			gifImage.addClass("gif");
-			gifImage.attr("src", response[i].images.fixed_height_still.url);
-			gifImage.attr("data-still", response[i].images.fixed_height_still.url);
-			gifImage.attr("data-animate", response[i].images.fixed_height.url);
-			gifImage.attr("data-state", "still");
+	//centre text for the item's rating
+	p.addClass("text-center");
 
-			// places the image and the rating text in the gifDiv
-			gifDiv.append(p);
-			gifDiv.prepend(gifImage);
+	// Creating and storing a img tag-(6.3-DynamicElements)
+	var gifImage = $("<img>");
+	// Setting different attribute of the gifimage to a property pulled off the result item
+	gifImage.attr("gif");
+	gifImage.attr("src", results[i].images.fixed_height_still.url);
+	gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+	gifImage.attr("data-animate", results[i].images.fixed_height.url);
+	gifImage.attr("data-state", "still");
 
-			// places the gifDiv at the top of the mainArea div
-			$("#mainArea").prepend(gifDiv);
+	// Appending the line and animation to the gifDiv
+	gifDiv.append(p);
+	gifDiv.append(gifImage);
+
+	// Prepending the gifDiv to HTML
+	$("#mainArea").prepend(gifDiv);
 		}
 	});
-}
+	}
 
-// when the submit button is clicked, the input value is pushed to the tvShows array and rendered into a new button
-$("#submit-btn").on("click", function(event) {
+	// This function handles events where a movie button is clicked and grabs the input from textbox
+	$("#submit-btn").on("click", function (event) {
 	event.preventDefault();
 
 	var newShow = $("#userInput").val().trim();
 	tvShows.push(newShow);
-	renderButtons();
+	buttons();
 });
 
-// listens for a click of any button with an id of tv-btn, then performs the displayGifs function
-$(document).on("click", "#tv-btn", displayGifs);
+	// displayGifs function
+	$(document).on("click", "#tv-btn", displayGifs);
 
-// starts and stops the animated gif on click
-$(document).on("click", ".gif", function() {
+	// starts and stops the animated gif on click
+	$(document).on("click", ".gif", function () {
 	var state = $(this).attr("data-state");
+
+	// If the clicked image's state is still, update its src attribute to what its data-animate value is.
+	// Then, set the image's data-state to animate
+	// Else set src to the data-still value-(6.3-pausing-gifs)
 
 	if (state === "still") {
 		$(this).attr("src", $(this).attr("data-animate"));
@@ -78,4 +91,4 @@ $(document).on("click", ".gif", function() {
 	}
 });
 
-renderButtons();
+buttons();
